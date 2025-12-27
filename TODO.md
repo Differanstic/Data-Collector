80e46d02ca4bae55bcd7d39fd9cddfbbbed066ea
Kathan Pandya <kathancpandya@gmail.com>
2025-12-27 13:30:21 +0530

    TODOS

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# Project TODOs
+
+## Feature Adding
+- [ ] Add Discord Messaging API.
+- [ ] TBT Market Depth On NSE.
+  
+## Issue Handling
+- [ ] CSV Logger Fall Back Logic.
+- [ ] Sometimes it doesnt auto close even after market is closed.
--- src/data_collector/main.py
+++ src/data_collector/main.py
@@ -1,24 +1,11 @@
-from data_collector.symbol_management.fyers_symbols import fyers_symbols
 from Quantlib.fyers_util import fyers_util
-from prettytable import PrettyTable
 from data_collector.websocket.fyers_ws import fyers_ws
-import time
-import threading
 
-
-def main():
-    #start = time.time()   
-    #symbols = fyers_symbols(fyers_util(),load_stock_fno=1).load_symbols()
-    #table = PrettyTable(['Symbol','Path'])
-    #table.add_rows([[k, v] for k, v in symbols.items()])
-    #print(f'Time Took: {time.time() - start}')
-    #print(len(symbols))
-    #print(table)
-    
+def main():    
     fyers_ws(fyers_util()).connect()
-    while 1:
-        time.sleep(1)
-    print("done")
+    #while 1:
+    #    time.sleep(1)
+    #print("done")
 
 
 
